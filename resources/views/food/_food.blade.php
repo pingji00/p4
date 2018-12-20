1be2294fd7de0bccf673150245678ea9d7e2b29f
pingji00 <pingji@me.com>
2018-12-19 23:00:23 -0500

    add new tag for added food, it will expire in 30s

--- resources/views/food/_food.blade.php
+++ resources/views/food/_food.blade.php
@@ -1,6 +1,6 @@
 <td><a href="/foods/{{ $food->id }}">{{$food->name}}</a>
-{{--if the food was created within 10 mins(600s), the new tag will show--}}
-@if ( strtotime(Carbon\Carbon::now()) - strtotime($food->created_at) < 600 )
+{{--if the food was created within 30s, the new tag will show--}}
+@if ( strtotime(Carbon\Carbon::now()) - strtotime($food->created_at) < 30 )
 <div class='new'>new</div>
 @endif
 </td>
